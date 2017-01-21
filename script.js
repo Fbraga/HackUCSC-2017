7916bfdaa5ddcfb5ccc337cd86a6ac995cd655d4
Francisco Braga <franciscobraga@eduroam-169-233-239-212.ucsc.edu>
2017-01-21 13:51:33 -0800

    changed zoom to 14 on script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function myMap() {
   var mapCanvas = document.getElementById("map");
   var mapOptions = {
     center: new google.maps.LatLng(36.9914, -122.0609), 
-    zoom: 4,
+    zoom: 14,
   }
   var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
+          zoom: 14,
           center: myLatLng
   });
 
