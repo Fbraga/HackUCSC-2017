349f1428b4852e697ff24c95abd7fcb43cc22328
Francisco Braga <franciscobraga@eduroam-169-233-239-212.ucsc.edu>
2017-01-21 13:49:41 -0800

    Added marker to script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 
 function myMap() {
+  var myLatLng = {lat: 36.9914, lng: -122.0609}
   var mapCanvas = document.getElementById("map");
   var mapOptions = {
     center: new google.maps.LatLng(36.9914, -122.0609), 
-    zoom: 14
+    zoom: 4,
   }
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  
+  var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+  });  
 }
 
