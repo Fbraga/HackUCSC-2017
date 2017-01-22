720bbf576c9093bbdd7d7bed2e5bd2640726e15a
Francisco Braga <franciscobraga@eduroam-169-233-239-212.ucsc.edu>
2017-01-21 16:56:04 -0800

    Updated landmark and index

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express();
+
+// GET method route
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage')
+})
+
+// POST method route
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage')
+})
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+    res.send(req.params)
+})
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
+})
+
+
