ceda5a4e9da2c2e16a691866eba32a3227a7a6f0
Thomas Shum <jabashque@gmail.com>
2017-01-22 08:04:02 +0000

    asyncronous garbage

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ var assert = require('assert')
 var interact = require('./lib/mongointeract.js')
 var app = express()
 
-MongoClient.connect('mongodb://loclahost:27017/myproject', function(err, db) {
+MongoClient.connect('mongodb://localhost:27017/myproject', function(err, db) {
   assert.equal(null, err)
   console.log("connect successful")
 
   app.use(express.static('.'))
 
-  app.get('get_names', function(req, res) {
+  app.get('/get_names', function(req, res) {
     res.send(interact.get_names(db))
   })
 
-  app.get('get/:name', function(req, res) {
+  app.get('/get/:name', function(req, res) {
     res.send(interact.get(db, req))
   })
 
-  app.get('add/:lat/:lon/:name/:description', function(req, res) {
+  app.get('/add/:lat/:lon/:name/:description', function(req, res) {
     interact.add(db, req)
   })
 
-  app.get('debug/:lat/:lon/', function(req, res) {
+  app.get('/debug/:lat/:lon/', function(req, res) {
     var dummy = {
       "name" : "foobar",
       "lat" : parseFloat(req.params["lat"]),
