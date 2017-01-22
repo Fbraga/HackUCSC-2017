8431bc41f9a6afdd2901e0cb363a88233e5d6904
Francisco Braga <franciscobraga@eduroam-169-233-239-212.ucsc.edu>
2017-01-21 17:25:29 -0800

    Added a dummy list to get front end talking with back end.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express')
 var app = express();
 
+app.use(express.static('.'))
+
+app.get('/:lat/:lon/', function(req, res) {
+  var dummy = {
+    "name" : "foobar",
+    "lat" : parseInt(req.params["lat"]),
+    "lon" : parseInt(req.params["lon"]),
+    "rating" : 5
+  }
+  res.send(dummy)
+})
 // GET method route
+/*
 app.get('/', function (req, res) {
     res.send('GET request to the homepage')
 })
-
+*/
 // POST method route
 app.post('/', function (req, res) {
     res.send('POST request to the homepage')
